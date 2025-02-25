7c0ef3f788b83e692dea710b3e07a31d6379f8a9
DaviCortesDev <davicortes1@hotmail.com>
2025-02-25 16:34:06 -0300

    Cadastro feito

--- projeto_p3/dreamdiary/views.py
+++ projeto_p3/dreamdiary/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from dreamdiary.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def login (request):
     return render(request, 'pages/login.html')
 
-def cadastro (request):
-    return render(request, 'pages/cadastro.html')
+def cadastro(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        # Verifica se o usuário já existe
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Usuário já cadastrado!")
+            return redirect("cadastro")
+
+        # Criar o usuário
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        messages.success(request, "Cadastro realizado com sucesso! Faça login.")
+        return redirect("login")
+
+    return render(request, "pages/cadastro.html")
 
 def home (request):
     return render(request, 'pages/home.html')
